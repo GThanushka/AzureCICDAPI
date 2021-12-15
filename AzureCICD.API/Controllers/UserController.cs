eb7c9214f5c406e166b263fa2d43db89e62694d9
Thanushka Gunasekara <tgunasekara@SL-TGUNASEKARAA>
2021-12-15 11:25:45 +0530

    Initial project setup

---
+++ AzureCICD.API/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AzureCICD.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public string SayHello()
+        {
+            return "Hello World";
+        }
+    }
+}
